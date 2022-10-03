ff46350a76d5cd0d4251dad31fbf9df707e59ae7
Houre Souren <112374244+houresouren81@users.noreply.github.com>
2022-10-03 21:12:31 +0200

    lagt in  sms och console

---
+++ script.js
@@ -0,0 +1,6 @@
+const sms=document.getElementById("sms");
+const smsbtn=document.getElementById("smsbtn");
+
+smsbtn.addEventListener("click",function()
+{console.log("click på knapp");
+console.log(sms.value)})
